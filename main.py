9bdb8cde13f43c01c3cce0a671af31e5804415e0
kolya <kolanakolana1@gmail.com>
2021-12-07 12:39:16 +0300

    ver 1

--- main.py
+++ main.py
@@ -6,30 +6,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def translater():
-    native = ''
-    trans = ''
-    lang = ''
-    lang_edited = ''
-    translated_message = ''
-    code_dict = {'1': 'ru', '2': 'en', '3': 'es', '4': 'fr', '5': 'de'}
-
     translator = Translator()
 
     if request.method == 'POST':
         trans = request.form.get('trans')  # запрос к данным формы
         lang = request.form.get('lang')
-        print(trans, code_dict[lang])
-        if lang in code_dict.keys():
-            lang_edited = code_dict[lang]
-            translation = translator.translate(trans, dest=lang_edited)
-            translated_message = translation.text
-            return render_template('index.html', my_string=translated_message)
+        translation = translator.translate(trans, dest=lang)
+        translated_message = translation.text
+        return render_template('index.html', my_string=translated_message)
 
     return render_template('index.html')
 
 
-
-#output: 'The sky is blue and I like bananas'
-
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run()
